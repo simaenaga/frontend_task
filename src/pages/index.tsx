5d99a4b2c51e05997d1b2ba12c094579289d52b6
simaenaga <tamie.taniguchi@gmail.com>
2019-11-15 22:58:45 +0900

    typescript導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import * as React from "react";
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
@@ -6,7 +6,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-class BlogIndex extends React.Component {
+interface BlogIndexProps {
+}
+class BlogIndex extends React.Component<BlogIndexProps> {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
